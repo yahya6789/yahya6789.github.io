131af2b15a797f72c5e17d6a987368370872f6ee
Yahya <yahya6789@gmail.com>
2020-08-19 11:37:21 +0700

    Merge JS.

--- navbar.js
+++
@@ -1,5 +0,0 @@
-const burger = document.getElementById('navbar-burger');
-burger.addEventListener('click', () => {
-    burger.classList.toggle('is-active');
-    document.getElementById(burger.dataset.target).classList.toggle('is-active');
-});
